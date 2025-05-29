994a030ece16898b84c63bc755d4aee65cad0af1
PedrowjCA <pedrojos.campos@protonmail.com>
2025-05-29 16:01:31 -0300

    refactor game start and gamemode selection

--- src/main/java/dcc025/PedroJoseCA/game/Game.java
+++ src/main/java/dcc025/PedroJoseCA/game/Game.java
@@ -7,25 +7,31 @@ public class Game {
 
     public void start() {
         System.out.println("Welcome to Duels");
-        selectGameMode();
+        numberOfPlayers = selectGameMode();
+        switch (numberOfPlayers) {
+            case 1:
+                playSinglePlayer();
+                break;
+            case 2:
+                playMultiPlayer();
+                break;
+        }
     }
 
-    private void selectGameMode() {
-        System.out.println("Select a game mode");
-        System.out.println("Digit 1 for single player");
-        System.out.println("Digit 2 for multiplayer");
-        Scanner keyboard = new Scanner(System.in);
-        int selectedGameMode = keyboard.nextInt();
-        if (selectedGameMode == 1) {
-            numberOfPlayers = selectedGameMode;
-            playSinglePlayer();
-        } else if (selectedGameMode == 2) {
-            numberOfPlayers = selectedGameMode;
-            playMultiPlayer();
-        } else {
-            System.out.println("Invalid game mode. Try again.");
-            selectGameMode();
-        }
+    private int selectGameMode() {
+        int selectedGameMode;
+        do {
+            System.out.println("Select a game mode");
+            System.out.println("Digit 1 for single player");
+            System.out.println("Digit 2 for multiplayer");
+            Scanner keyboard = new Scanner(System.in);
+            selectedGameMode = keyboard.nextInt();
+            if (selectedGameMode != 1 && selectedGameMode != 2) {
+                System.out.println("Invalid game mode. Try again.");
+            }
+        } while (selectedGameMode != 1 && selectedGameMode != 2);
+
+        return selectedGameMode;
     }
 
     private void playSinglePlayer() {
@@ -37,6 +43,5 @@ public class Game {
     }
 
     private void chooseCharacters() {
-
     }
 }
